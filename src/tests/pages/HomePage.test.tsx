025cd10be3f320fd75729fad1bd548b4ca0343f3
tymchijal <tymchijal@iek.ru>
2025-03-03 11:30:38 +0300

    tests

--- src/tests/pages/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import DashboardPage from '../../pages/Dashboard'
-
-test('renders hello world message', () => {
-    render(<DashboardPage />)
-    const greetings = screen.getByText(/Hello world/i)
-    expect(greetings).toBeInTheDocument()
-})
--- src/tests/pages/NotFoundPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import NotFoundPage from '../../pages/NotFoundPage'
-import React from 'react'
-
-test('renders error message', () => {
-    render(<NotFoundPage />)
-    const errorMessage = screen.getByText(/Oops 404!/i)
-    expect(errorMessage).toBeInTheDocument()
-})
